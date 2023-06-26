0ee0e0990bfb3d56703044c8b07b22315942b779
sivaShilpa <shilpa.vchand@gmail.com>
2023-06-26 12:53:09 -0400

    2

--- 2.js
+++ 2.js
@@ -15,57 +15,58 @@ class ListNode{
         this.next = (next===undefined ? null : next)       
     }
 }
-// class LinkedList {
-//     constructor(node, next)
-//     {
-//         this.node = (node===undefined ? null : node)
-//         this.next = (next===undefined ? null : next)
-//         this.head = null
-//         this.size = 0
-//     }
- 
-//     // functions to be implemented
-//     // add(element)
-//     // insertAt(element, location)
-//     // removeFrom(location)
-//     // removeElement(element)
- 
-//     // Helper Methods
-//     // isEmpty
-//     // size_Of_List
-//     // PrintList
-// }
+
 var addTwoNumbers = function(l1, l2) {
     let result = new ListNode()
-    let walker3
+    let walker3 = result
     let walker1 = l1
     let walker2 = l2
-    let size = 0
-    let temp = 0
+    let carry = 0
+    let size1 = 0
+    let size2 = 0
+    
+    while(walker1.next){
+        size1++
+        walker1 = walker1.next
+    }
+
+    while(walker2.next){
+        size2++
+        walker2 = walker2.next
+    }
+    
+    if(size1>size2){
+        let diff = size1 - size2
+        let i = 0
+        while(i<diff){
+            walker2.next = new ListNode()
+            walker2 = walker2.next
+            i++
+        }
+    }else if(size1<size2){
+        let diff = size2 - size1
+        let i = 0
+        while(i<diff){
+            walker1.next = new ListNode()
+            walker1 = walker1.next
+            i++
+        }
+
+    }
+    
+    walker1 = l1
+    walker2 = l2
 
     while(walker1 && walker2){
-        temp += walker1.val + walker2.val
-        if(temp.toString().length === 1){
-            walker3 = new ListNode(temp)
-            console.log(walker3)
-            if(size === 0){
-                result = walker3
-            }
-            size++
-
-        }else{
-            let tempArr = temp.toString().split("")
-            walker3= new ListNode(Number(tempArr[1]))
-            console.log(walker3)
-            temp = Number(tempArr[0])
-            if(size === 0){
-                result = walker3
-            }
-            size++
-        }
+        walker3.val = walker1.val + walker2.val + carry
+        carry = Math.floor(walker3.val/10)
+        walker3.val = walker3.val%10
         walker1 = walker1.next
         walker2 = walker2.next
-        walker3 = walker3.next
+        if(walker1 || walker2 || carry!==0){
+            walker3.next = new ListNode(carry)
+            walker3 = walker3.next
+        }
     }
 
     return result    
@@ -74,6 +75,7 @@ var addTwoNumbers = function(l1, l2) {
 const l1 = new ListNode(2)
 l1.next = new ListNode(4)
 l1.next.next = new ListNode(3)
+l1.next.next.next = new ListNode(9)
 
 const l2 = new ListNode(5)
 l2.next = new ListNode(6)
