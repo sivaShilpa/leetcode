8b5db49632f8122eed9f672892206821bc964078
sivaShilpa <shilpa.vchand@gmail.com>
2023-08-10 17:04:22 -0400

    125

---
+++ 125.js
@@ -0,0 +1,18 @@
+var isPalindrome = function(s) {
+    let newStr = s.replace(/[^a-z0-9]/gi, '')
+    let retVal = false
+
+    if(newStr === "" || newStr.length === 1) return true
+    
+    for(let i = 0; i <= (newStr.length/2)-1; i++){
+        if(newStr[i].toLowerCase() === newStr[newStr.length - 1 - i].toLowerCase()){  
+            retVal = true
+        }else{
+            retVal = false 
+            break
+        }
+    }
+    return retVal
+};
+
+console.log(isPalindrome("race a car"))
